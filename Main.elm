cb30d85e3006372f5c9394e146db9c04d8bb209c
Zach Rose <zach.rose@chromeriver.com>
2017-08-11 12:51:49 -0700

    Implement match winning

--- Main.elm
+++ Main.elm
@@ -11,16 +11,21 @@ main =
 
 -- MODEL
 
+type Player
+  = Player1
+  | Player2
+
 type alias Model =
   { player1GameScore : Int
   , player2GameScore : Int
   , player1MatchScore : Int
   , player2MatchScore : Int
+  , matchWinner : Maybe Player
   }
 
 initialModel : Model
 initialModel =
-  Model 0 0 0 0
+  Model 0 0 0 0 Nothing
 
 
 -- UPDATE
@@ -30,9 +35,14 @@ type Msg
   | Player2Point
   | Reset
 
-type Player
-  = Player1
-  | Player2
+matchWinner : Model -> Maybe Player
+matchWinner model =
+  if model.player1MatchScore == 2 || model.player1MatchScore == 1 && (gameWinner model) == Just Player1 then
+    Just Player1
+  else if model.player2MatchScore == 2 || model.player2MatchScore == 1 && (gameWinner model) == Just Player2 then
+    Just Player2
+  else
+    Nothing
 
 gameWinner : Model -> Maybe Player
 gameWinner model =
@@ -43,33 +53,58 @@ gameWinner model =
   else
     Nothing
 
+winnerText : Model -> String
+winnerText model =
+  case model.matchWinner of
+    Just Player1 -> "Congrats Player 1"
+    Just Player2 -> "Good Job Player 2"
+    Nothing -> ""
+
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Player1Point ->
-      let
-        intermediateModel = { model | player1GameScore = model.player1GameScore + 1 }
-      in
-      case gameWinner intermediateModel of
-        Just Player1 ->
-          Model 0 0 ( model.player1MatchScore + 1) model.player2MatchScore
-        Just Player2 ->
-          Model 0 0 model.player1MatchScore ( model.player2MatchScore + 1)
-        Nothing ->
-          intermediateModel
-    Player2Point ->
-      let
-        intermediateModel = { model | player2GameScore = model.player2GameScore + 1 }
-      in
-      case gameWinner intermediateModel of
-        Just Player1 ->
-          Model 0 0 ( model.player1MatchScore + 1) model.player2MatchScore
-        Just Player2 ->
-          Model 0 0 model.player1MatchScore ( model.player2MatchScore + 1)
-        Nothing ->
-          intermediateModel
-    Reset ->
-      initialModel
+  case model.matchWinner of
+    Just Player1 ->
+      model
+    Just Player2 ->
+      model
+    Nothing ->
+      case msg of
+        Player1Point ->
+          let
+            intermediateModel = { model | player1GameScore = model.player1GameScore + 1 }
+          in
+          case matchWinner intermediateModel of
+            Just Player1 ->
+              { intermediateModel | matchWinner = Just Player1 }
+            Just Player2 ->
+              { intermediateModel | matchWinner = Just Player2 }
+            Nothing ->
+              case gameWinner intermediateModel of
+                Just Player1 ->
+                  Model 0 0 ( model.player1MatchScore + 1) model.player2MatchScore Nothing
+                Just Player2 ->
+                  Model 0 0 model.player1MatchScore ( model.player2MatchScore + 1) Nothing
+                Nothing ->
+                  intermediateModel
+        Player2Point ->
+          let
+            intermediateModel = { model | player2GameScore = model.player2GameScore + 1 }
+          in
+          case matchWinner intermediateModel of
+            Just Player1 ->
+              { intermediateModel | matchWinner = Just Player1 }
+            Just Player2 ->
+              { intermediateModel | matchWinner = Just Player2 }
+            Nothing ->
+              case gameWinner intermediateModel of
+                Just Player1 ->
+                  Model 0 0 ( model.player1MatchScore + 1) model.player2MatchScore Nothing
+                Just Player2 ->
+                  Model 0 0 model.player1MatchScore ( model.player2MatchScore + 1) Nothing
+                Nothing ->
+                  intermediateModel
+        Reset ->
+          initialModel
 
 -- VIEW
 
@@ -77,6 +112,10 @@ view : Model -> Html Msg
 view model = 
   Html.div []
   [
+    Html.div [ id "winner" ]
+    [
+      Html.h3 [] [ Html.text (winnerText model) ]
+    ],
     Html.div [ id "player1" ]
     [
       Html.h1 [] [ Html.text "p1" ],
